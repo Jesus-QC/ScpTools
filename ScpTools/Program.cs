2982f142d33ace1e2324896cd43300685d73bc91
Jesus-QC <jesusquirocampos@gmail.com>
2022-07-12 19:58:28 +0200

    closing div

--- ScpTools/Program.cs
+++ ScpTools/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
